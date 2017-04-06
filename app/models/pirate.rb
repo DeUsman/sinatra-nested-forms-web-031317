4126af2d285f070c08726cd696ca7a3c11105fe6
Usman Pervaiz <de.usman@yahoo.com>
2017-04-06 15:03:08 -0400

    Done

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+	attr_accessor :name, :weight, :height
+	ALL = []
+	def initialize(hash)
+		@name = hash[:name]
+		@weight = hash[:weight]
+		@height = hash[:height]
+		ALL << self
+	end
+
+	def self.all
+		ALL
+	end
+
 end
